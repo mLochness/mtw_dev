1188ca36f6374075c078f4eb83f51a0def6c3d61
mLochness <lochness@sernato.sk>
2024-06-06 01:24:51 +0200

    step 2

--- script.js
+++ script.js
@@ -5,21 +5,22 @@ jQuery(document).ready(function ($) {
 
   //let paraSectionsCount = document.querySelector(".para-section").length;
   let paraSectionsCount = $(".para-section").length;
-  let paralaxMaxVoffset;
+  let scrollIndexMax;
   let prlxContainer = $("#parallax-container");
   let prlxLayers = $(".prlxLayer");
   let prlxLayersCount = $(".prlxLayer").length;
-  let prlxSpeed = 10 //parallax speed
-  let scrollIndex = 0;
-  let currentPosition = 0;
-  let contentLayer = $("#contentLayer");
   let contentLayerIndex = $("#contentLayer").index();
+  let prlxRatio = 1 / contentLayerIndex //parallax speed according to content layer
+  let scrollIndex = 0;
+  //let currentPosition = 0;
+  //let contentLayer = $("#contentLayer");
+
   let moveUnit;
+  let centUnit;
   //let pushLimit;
 
   console.log("contentLayerIndex:", contentLayerIndex);
   console.log("prlxLayersCount:", prlxLayersCount);
-
 
   getWindowSize();
   //document.querySelector(".para-section").classList.add("current");
@@ -31,85 +32,99 @@ jQuery(document).ready(function ($) {
     vWidth = document.documentElement.clientWidth;
     document.documentElement.style.setProperty("--vWidth", `${vWidth}px`);
 
-    paralaxMaxVoffset = (paraSectionsCount - 1) * vWidth;
-    // set the sensitivity of scroll takeover
-    //pushLimit = vWidth / 10;
+    scrollIndexMax = -100 * (paraSectionsCount - 1);
 
     // set top parallax layer width:
-    //$(".prlxLayer:last").css("width", "" + paraSectionsCount * vWidth * 1.5 + "px");
+    $(".prlxLayer:last").css("width", "" + paraSectionsCount * vWidth * 1.5 + "px");
     // count moveUnits for fitting sections in viewport:
     moveUnit = vWidth / paraSectionsCount;
-    //sectionUnit = moveUnit * contentLayerIndex;
+    centUnit = vWidth / 100;
     console.log("window width/height:", vWidth, "/", vHeight);
-    console.log("paraSectionsCount:", paraSectionsCount, "paralaxMaxVoffset:", paralaxMaxVoffset);
+    console.log("paraSectionsCount:", paraSectionsCount, "scrollIndexMax:", scrollIndexMax);
   }
+
+function throttle(fn, wait) {
+  var time = Date.now();
+  return function() {
+    if ((time + wait - Date.now()) < 0) {
+      fn();
+      time = Date.now();
+    }
+  }
+}
+
+
   // fired wheel/touchmove or swipe events counter
   var triggers = 0;
+
   // position reference container id: #contentLayer
   $(prlxContainer).on("wheel touchmove", function (e) {
+    
     ++triggers;
-
-    //const y = e.deltaY;
     const y = e.originalEvent.deltaY;
     const x = e.originalEvent.deltaX;
-    currentPosition = $("#contentLayer").position().left;
+    //currentPosition = $("#contentLayer").position().left;
     current = $(".current");
-    curSectionIndex = $(".current").index() + 1;
+    curSectionIndex = $(".current").index();
+    let thisIndex;
+
 
     if (y < 0 || x < 0) {
       scrollIndex++;
       if (scrollIndex > 0) {
         scrollIndex = 0;
       }
-      if (y < 1 && triggers > 5 ) {
+      if (y < 1 && triggers > 7) {
         console.log("triggers limit, function LEFT fired -------------");
         triggers = 0;
         if ($(current).prev().length) {
           var prevSection = $(current).prev();
           $(prlxLayers).each(function () {
-            let thisIndex = $(this).index();
+            thisIndex = $(this).index();
             $(this).css("transform", "translateX(" + (moveUnit * (thisIndex + 1) * -1 * (curSectionIndex - 1)) + "px)");
-            console.log("curSectionIndex - 2:", curSectionIndex - 2);
+            console.log("curSectionIndex - 1:", curSectionIndex - 1);
           });
+          scrollIndex = -100 * (curSectionIndex - 1);
           $(current).removeClass("current");
           $(prevSection).addClass("current");
+        }
+        else {
+          scrollIndex = 0;
         }
       }
     }
     if (y > 0 || x > 0) {
       scrollIndex--;
-      if (y > 1 && triggers > 5) {
+      if (y > 1 && triggers > 7) {
         console.log("triggers limit, function RIGHT fired -------------");
         triggers = 0;
         if ($(current).next().length) {
           var nextSection = $(current).next();
           $(prlxLayers).each(function () {
-            let thisIndex = $(this).index();
-            $(this).css("transform", "translateX(" + (-moveUnit * (thisIndex + 1) * curSectionIndex) + "px)");
+            thisIndex = $(this).index();
+            $(this).css("transform", "translateX(" + (-moveUnit * (thisIndex + 1) * (curSectionIndex + 1)) + "px)");
           });
+          scrollIndex = -100 * (curSectionIndex + 1);
           $(current).removeClass("current");
           $(nextSection).addClass("current");
         }
       }
     }
-    $(prlxLayers).each(function () {
-      let thisIndex = $(this).index();
-      $(this).css("transform", "translateX(" + (scrollIndex * thisIndex * prlxSpeed) + "px)");
-    });
-
-
-
-    // for (let i = 0; i < prlxLayers.length; i++) {
-    //   //prlxLayers[i].style.transform = 'translateX(-' + (window.pageYOffset * i / prlxLayers.length) + 'px)';
-    //   prlxLayers[i].style.transform =
-    //     "translateX(" + ((scrollIndex * i * 20) / prlxLayers.length) + "px)";
+    scrollTrace = scrollIndex * centUnit;
+    if (scrollIndex > scrollIndexMax) {
+      $(prlxLayers).each(function () {
+        thisIndex = $(this).index();
+        $(this).css("transform", "translateX(" + (scrollTrace * thisIndex * prlxRatio) + "px)");
+      });
+    }
+    // if (scrollIndex < scrollIndexMax) {
+    //   scrollIndex = scrollIndexMax;
     // }
-    // console.log("deltaY:", y, "deltaX:", x);
+
+    console.log("triggers:", triggers);
     console.log("scrollIndex:", scrollIndex);
-    // console.log("currentPosition:", currentPosition);
+    console.log("scrollTrace:", scrollTrace);
   });
-
-
 
   // *************************************************************
 
@@ -141,7 +156,6 @@ jQuery(document).ready(function ($) {
         clearTimeout(timer);
         timer = null;
       }
-
       if (counter >= options.stack) {
         _call(arguments);
       } else {
@@ -158,6 +172,7 @@ jQuery(document).ready(function ($) {
   var holdResize = asyncProxy(
     function (event) {
       getWindowSize();
+      alignSectionAfterResize();
     },
     {
       delay: 250
@@ -166,5 +181,15 @@ jQuery(document).ready(function ($) {
 
   $(window).on("resize", holdResize);
 
+  function alignSectionAfterResize() {
+    curSectionIndex = $(".current").index();
+    $(prlxLayers).each(function () {
+      let thisIndex = $(this).index();
+      $(this).css("transform", "translateX(" + (-moveUnit * (thisIndex + 1) * curSectionIndex) + "px)");
+      console.log("aligned to ", "(" + (-moveUnit * (thisIndex + 1) * curSectionIndex) + "px)")
+    });
+  }
+
 })
+
 
